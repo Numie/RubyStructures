0014e7e437cd68c90a7f26d9c82098d9c3fc1937
Jason Numeroff <jnumeroff@hotmail.com>
2018-05-06 22:44:10 -0400

    Implement Stack

---
+++ stack.rb
@@ -0,0 +1,53 @@
+class Stack
+  def initialize
+    @store = Array.new
+  end
+
+  def to_a
+    Array.new(@store)
+  end
+
+  def to_s
+    @store
+  end
+
+  def inspect
+    @store
+  end
+
+  def ==(other_stack)
+    @store == other_stack.send(:store)
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def push(el)
+    @store.push(el)
+  end
+
+  def <<(el)
+    @store << el
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def peek
+    @store.last
+  end
+
+  def length
+    @store.length
+  end
+
+  def include?(el)
+    @store.include?(el)
+  end
+
+  private
+
+  attr_reader :store
+end
