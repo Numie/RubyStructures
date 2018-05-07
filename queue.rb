df77b13917632900b2151ce6e12b2930f33dc252
Jason Numeroff <jnumeroff@hotmail.com>
2018-05-06 22:49:40 -0400

    Implement Queue

---
+++ queue.rb
@@ -0,0 +1,53 @@
+class Queue
+  def initialize
+    @store = Array.new
+  end
+
+  def to_a
+    Array.new(@store)
+  end
+
+  def to_s
+    @store
+  end
+
+  def inspect
+    @store
+  end
+
+  def ==(other_queue)
+    @store == other_queue.send(:store)
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def enqueue(el)
+    @store.push(el)
+  end
+
+  def <<(el)
+    @store << el
+  end
+
+  def dequeue
+    @store.shift
+  end
+
+  def peek
+    @store.first
+  end
+
+  def length
+    @store.length
+  end
+
+  def include?(el)
+    @store.include?(el)
+  end
+
+  private
+
+  attr_reader :store
+end
