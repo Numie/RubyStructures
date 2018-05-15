238bb5ee91356cf05141f9f87b259520016625e2
Jason Numeroff <jnumeroff@hotmail.com>
2018-05-14 22:22:17 -0400

    Add depth first search and breadth first search for graph class

--- lib/data_structures/graph.rb
+++ lib/data_structures/graph.rb
@@ -22,4 +22,43 @@ class Graph
     @adjacency_list[id1].delete(id2)
     @adjacency_list[id2].delete(id1)
   end
+
+  def depth_first_search(target_id, start_id=nil)
+    @memo = Set.new unless start_id
+    @memo ||= Set.new
+    current_vertex = start_id || @adjacency_list.first.first
+
+    current_adjacency_list = @adjacency_list[current_vertex]
+    current_adjacency_list.each do |vertex|
+      if @memo.include?(vertex)
+        next
+      else
+        @memo.add(vertex)
+      end
+
+      return true if vertex == target_id
+      result = depth_first_search(target_id, vertex)
+      return result if result
+    end
+
+    false
+  end
+
+  def breadth_first_search(target_id, start_id=nil)
+    start_id ||= @adjacency_list.first.first
+    vertex_queue = [start_id]
+
+    memo = Set.new
+    while true
+      current_vertex = vertex_queue.shift
+      next if memo.include?(current_vertex)
+
+      current_adjacency_list = @adjacency_list[current_vertex]
+      return false if current_adjacency_list.nil?
+      return true if current_adjacency_list.include?(target_id)
+
+      vertex_queue += current_adjacency_list.to_a
+      memo.add(current_vertex)
+    end
+  end
 end
