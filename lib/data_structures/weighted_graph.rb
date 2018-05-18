6747f99c8fc5f8039095c42cf7c3b03f5c870b97
Jason <jnumeroff@hotmail.com>
2018-05-17 20:04:20 -0400

    Add highest and lowest weight adjacent methods for Weighted Graph class

--- lib/data_structures/weighted_graph.rb
+++ lib/data_structures/weighted_graph.rb
@@ -24,4 +24,48 @@ class WeightedGraph
     @adjacency_list[id1].delete(id2)
     @adjacency_list[id2].delete(id1)
   end
+
+  def adjacent?(id1, id2)
+    raise RuntimeError.new("#{id1} is not a Vertex") unless @adjacency_list.include?(id1)
+    raise RuntimeError.new("#{id2} is not a Vertex") unless @adjacency_list.include?(id2)
+    @adjacency_list[id1].include?(id2)
+  end
+
+  def adjacent_vertices(id)
+    raise RuntimeError.new("#{id} is not a Vertex") unless @adjacency_list.include?(id)
+    @adjacency_list[id]
+  end
+
+  def heighest_weight_adjacent(id)
+    adjacent_vertices = self.adjacent_vertices(id)
+    return nil if adjacent_vertices.empty?
+
+    highest = nil
+    highest_weight = nil
+    adjacent_vertices.each do |vertex, weight|
+      if highest_weight == nil || weight > highest_weight
+        highest = vertex
+        highest_weight = weight
+      end
+    end
+
+    highest
+  end
+
+  def lowest_weight_adjacent(id)
+    adjacent_vertices = self.adjacent_vertices(id)
+    return nil if adjacent_vertices.empty?
+
+    lowest = nil
+    lowest_weight = nil
+    adjacent_vertices.each do |vertex, weight|
+      if lowest_weight == nil || weight < lowest_weight
+        lowest = vertex
+        lowest_weight = weight
+      end
+    end
+
+    lowest
+  end
+
 end
